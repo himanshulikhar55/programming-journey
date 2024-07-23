e3c1c4a17c12b252ec7deee5f59e4da46010a45c
himanshulikhar55 <himanshulikhar55@gmail.com>
2024-07-23 22:11:24 +0530

    Old

--- striver-a2z-dsa-problems/linked-lists/int-of-two-ll.cpp
+++ striver-a2z-dsa-problems/linked-lists/int-of-two-ll.cpp
@@ -9,78 +9,39 @@ struct ListNode{
     }
 };
 /*
-    Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+    Given the heads of two singly linked-lists headA and headB,
+    return the node at which the two lists intersect.
+    If the two linked lists have no intersection at all, return null.
 */
-bool isPalindrome(ListNode* head) {
-    ListNode *slow = head, *fast = head;
-    while(fast && fast->next){
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    //now reverse the second-half of the LL
-    ListNode *curr=slow, *prev=NULL, *next=slow->next;
-    while(curr){
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-        if(curr)
-            next = curr->next;
-    }
-    ListNode *newHead = prev;
-    while(head && newHead){
-        if(head->val == newHead->val){
-            head = head->next;
-            newHead = newHead->next;
-        } else {
-            return false;
-        }
-    }
-    return true;
-}
-/*
-    The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing,
-    where ⌊x⌋ denotes the largest integer less than or equal to x.
-    For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
-*/
-ListNode* deleteMiddle(ListNode* head) {
-    if(!head->next)
-        return NULL;
-    ListNode *slow=head, *fast=head, *prev;
-    while(fast && fast->next){
-        prev = slow;
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    prev->next = slow->next;
-    delete(slow);
-    return head;
-}
-void printLL(ListNode *head){
-    while(head){
-        cout << head->val << ' ';
-        head = head->next;
-    }
-    cout << '\n';
-}
 ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    
-}
-int main(){
-    int n, x;
-    cin >> n;
-    ListNode *head = NULL, *temp;
-    for(int i=0;i<n;i++){
-        cin >> x;
-        if(!head){
-            head = new ListNode(x);
-            temp = head;
-        } else {
-            temp->next = new ListNode(x);
-            temp = temp->next;
+    int len1=0, len2=0;
+    ListNode *temp1=headA, *temp2=headB;
+    while(temp1){
+        ++len1;
+        temp1 = temp1->next;
+    }
+    while(temp2){
+        ++len2;
+        temp2 = temp2->next;
+    }
+    temp1 = headA;
+    temp2 = headB;
+    if(len1>len2){
+        while(len1!=len2){
+            --len1;
+            temp1 = temp1->next;
+        }
+    } else{
+        while(len1!=len2){
+            --len2;
+            temp2 = temp2->next;
         }
     }
-    head = deleteMiddle(head);
-    cout << "LL after removing the middle element is: ";
-    printLL(head);
-    return 0;
+    while(temp1){
+        if(temp1 == temp2)
+            return temp1;
+        temp1=temp1->next;
+        temp2=temp2->next;
+    }
+    return temp1;
 }
